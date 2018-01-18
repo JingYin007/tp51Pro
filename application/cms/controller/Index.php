2625ee7e7108a637e714c876d6ff1fa196a73002
moTzxx <JingYin007@users.noreply.github.com>
2018-01-18 17:53:36 +0800

    新建controller

---
+++ application/cms/controller/Index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moTzxx
+ * Date: 2018/1/18
+ * Time: 17:52
+ */
+
+class Index
+{
+
+}
---
+++ application/cms/controller/NavMenus.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moTzxx
+ * Date: 2018/1/18
+ * Time: 17:53
+ */
+
+class NavMenus
+{
+
+}
