df90069cb22291e6683c1c0facb0ca7703b5c5c6
moTzxx <JingYin007@users.noreply.github.com>
2019-05-22 18:24:33 +0800

    ok

---
+++ public/Sina.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moTzxx
+ * Date: 2019/5/22
+ * Time: 17:31
+ */
+
+//namespace think;
+
+
+class Sina
+{
+  public function say(){
+      echo "hello Sina!";
+  }
+}
